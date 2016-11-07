86cd16b61b76625de42e0400e3fac4581f26139f
Gary Black <gar2000b@yahoo.com>
2016-11-07 14:49:19 -0500

    adding Arithmetic Progression + unit tests.

--- src/main/java/onlineinteract/com/progressions/ArithmeticProgression.java
+++ src/main/java/onlineinteract/com/progressions/ArithmeticProgression.java
@@ -3,5 +3,24 @@ package onlineinteract.com.progressions;
 /**
  * Created by Digilogue on 07/11/2016.
  */
-public class ArithmeticProgression {
+public class ArithmeticProgression extends Progression {
+
+    protected long increment;
+
+    public ArithmeticProgression() {
+        this(1, 0);
+    }
+
+    public ArithmeticProgression(long stepSize) {
+        this(stepSize, 0);
+    }
+
+    public ArithmeticProgression(long stepSize, long start) {
+        super(start);
+        increment = stepSize;
+    }
+
+    protected void advance() {
+        current += increment;
+    }
 }
