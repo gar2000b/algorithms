146eda4dfbe5ec542bd4eb2d5c31ac9443e12b8a
Gary Black <gar2000b@yahoo.com>
2016-11-07 14:47:21 -0500

    adding Arithmetic Progression + unit tests.

---
+++ src/main/java/onlineinteract/com/progressions/ArithmeticProgression.java
@@ -0,0 +1,7 @@
+package onlineinteract.com.progressions;
+
+/**
+ * Created by Digilogue on 07/11/2016.
+ */
+public class ArithmeticProgression {
+}
---
+++ src/test/java/onlineinteract/com/progressions/ArithmeticProgressionTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Digilogue on 07/11/2016.
+ */
+public class ArithmeticProgressionTest {
+
+}
