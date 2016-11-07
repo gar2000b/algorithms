3e378710f8160d9ff6bfb0431ad83377f1ef4f2e
Gary Black <gar2000b@yahoo.com>
2016-11-07 15:23:12 -0500

    adding Geometric Progression + unit tests.

---
+++ src/main/java/onlineinteract/com/progressions/GeometricProgression.java
@@ -0,0 +1,26 @@
+package onlineinteract.com.progressions;
+
+/**
+ * Created by Digilogue on 07/11/2016.
+ */
+public class GeometricProgression extends Progression {
+    protected long base;
+
+    public GeometricProgression() {
+        this(2, 1);
+    }
+
+    public GeometricProgression(long base) {
+        this(base, 1);
+    }
+
+    public GeometricProgression(long base, long start) {
+        super(start);
+        this.base = base;
+    }
+
+    protected void advance() {
+        current *= base;
+    }
+
+}
---
+++ src/test/java/onlineinteract/com/progressions/GeometricProgressionTest.java
@@ -0,0 +1,35 @@
+package onlineinteract.com.progressions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Digilogue on 07/11/2016.
+ */
+public class GeometricProgressionTest {
+
+    @Test
+    public void simpleGeometricProgressionWithDefaultBase2Start1Test() throws Exception {
+        GeometricProgression geometricProgression = new GeometricProgression();
+        assertEquals(1, geometricProgression.nextValue());
+        assertEquals(2, geometricProgression.nextValue());
+        assertEquals(4, geometricProgression.nextValue());
+    }
+
+    @Test
+    public void simpleGeometricProgressionWithBase4Start2Test() throws Exception {
+        GeometricProgression geometricProgression = new GeometricProgression(4, 2);
+        assertEquals(2, geometricProgression.nextValue());
+        assertEquals(8, geometricProgression.nextValue());
+        assertEquals(32, geometricProgression.nextValue());
+    }
+
+    @Test
+    public void simpleGeometricProgressionWithBase6DefaultStart1Test() throws Exception {
+        GeometricProgression geometricProgression = new GeometricProgression(6);
+        assertEquals(1, geometricProgression.nextValue());
+        assertEquals(6, geometricProgression.nextValue());
+        assertEquals(36, geometricProgression.nextValue());
+    }
+}
