e9b8b7f2581ac9653b0f81c4cc87d23dc706fc7b
Gary Black <gar2000b@yahoo.com>
2016-12-01 19:37:41 -0500

    added an optimized while

--- src/main/java/com/onlineinteract/strings/NeedInAHaystack.java
+++ src/main/java/com/onlineinteract/strings/NeedInAHaystack.java
@@ -3,25 +3,33 @@ package com.onlineinteract.strings;
 public class NeedInAHaystack {
 
 	public static void main(String[] args) {
-		int needleInHaystackPosition = getNeedleInAHaystack("azklneedleqbacszg", "needle");
-		System.out.println("Needle is at location: " + needleInHaystackPosition);
+		int needleInHaystackPosition = getNeedleInAHaystack(
+				"azklneedleqbacszg", "needle");
+		System.out
+				.println("Needle is at location: " + needleInHaystackPosition);
 	}
-	
+
+	/**
+	 * Function to check for a string within a string
+	 * 
+	 * @param haystack
+	 * @param needle
+	 * @return position in haystack where needle was found.
+	 */
 	protected static int getNeedleInAHaystack(String haystack, String needle) {
-		
+
 		int mainPos = 0;
-		
-		for(int i = 0; i < haystack.length(); i++) {
+
+		for (int i = 0; i < haystack.length(); i++) {
 			int hPos = mainPos;
 			int nPos = 0;
-			
+
 			for (int j = 0; j < needle.length(); j++) {
 				if (haystack.charAt(hPos) == needle.charAt(nPos)) {
 					hPos++;
 					nPos++;
-				} else {
+				} else
 					break;
-				}
 			}
 
 			if (nPos == needle.length())
@@ -29,7 +37,80 @@ public class NeedInAHaystack {
 			else
 				mainPos++;
 		}
-		
+
+		return -1;
+	}
+
+	/**
+	 * Function to check for a string within a string - this time using while
+	 * instead of for.
+	 * 
+	 * @param haystack
+	 * @param needle
+	 * @return position in haystack where needle was found.
+	 */
+	protected static int getNeedleInAHaystack2(String haystack, String needle) {
+
+		int mainPos = 0;
+		int i = 0;
+		while (i < haystack.length()) {
+			int hPos = mainPos;
+			int nPos = 0;
+
+			int j = 0;
+			while (j < needle.length()) {
+				if (haystack.charAt(hPos) == needle.charAt(nPos)) {
+					hPos++;
+					nPos++;
+				} else
+					break;
+
+				j++;
+			}
+
+			if (nPos == needle.length())
+				return mainPos;
+			else
+				mainPos++;
+
+			i++;
+		}
+
+		return -1;
+	}
+
+	/**
+	 * Function to check for a string within a string - optimisation of while
+	 * instead of for.
+	 * 
+	 * @param haystack
+	 * @param needle
+	 * @return position in haystack where needle was found.
+	 */
+	protected static int getNeedleInAHaystack3(String haystack, String needle) {
+
+		int mainPos = 0;
+		int i = 0;
+		while (i < haystack.length()) {
+			int hPos = mainPos;
+			int nPos = 0;
+
+			int j = 0;
+			while (j < needle.length()
+					&& haystack.charAt(hPos) == needle.charAt(nPos)) {
+				hPos++;
+				nPos++;
+				j++;
+			}
+
+			if (nPos == needle.length())
+				return mainPos;
+			else
+				mainPos++;
+
+			i++;
+		}
+
 		return -1;
 	}
 }
--- src/test/java/com/onlineinteract/strings/NeedInAHaystackTest.java
+++ src/test/java/com/onlineinteract/strings/NeedInAHaystackTest.java
@@ -10,31 +10,105 @@ public class NeedInAHaystackTest {
 	public void shouldFindNeedleAtPosition4() {
 		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack(
 				"azklneedleqbacszg", "needle");
-		
+
 		assertEquals(4, needleInHaystackPosition);
 	}
-	
+
 	@Test
 	public void shouldNotFindNeedle() {
 		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack(
 				"azklneeleqbacszg", "needle");
-		
+
 		assertEquals(-1, needleInHaystackPosition);
 	}
-	
+
 	@Test
 	public void shouldFindNeedleAtBeginning() {
 		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack(
 				"needleazklneelacsaeqbacszg", "needle");
-		
+
 		assertEquals(0, needleInHaystackPosition);
 	}
-	
+
 	@Test
 	public void shouldFindNeedleAtEnd() {
 		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack(
 				"neevsddleazklneelacsaeqbacszgneedle", "needle");
-		
+
+		assertEquals(29, needleInHaystackPosition);
+	}
+
+	/**
+	 * Same tests as above only targeting the while implementation of the
+	 * getNeedleInAHaystack() called getNeedleInAHaystack2().
+	 */
+
+	@Test
+	public void shouldFindNeedleAtPosition4_2() {
+		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack2(
+				"azklneedleqbacszg", "needle");
+
+		assertEquals(4, needleInHaystackPosition);
+	}
+
+	@Test
+	public void shouldNotFindNeedle_2() {
+		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack2(
+				"azklneeleqbacszg", "needle");
+
+		assertEquals(-1, needleInHaystackPosition);
+	}
+
+	@Test
+	public void shouldFindNeedleAtBeginning_2() {
+		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack2(
+				"needleazklneelacsaeqbacszg", "needle");
+
+		assertEquals(0, needleInHaystackPosition);
+	}
+
+	@Test
+	public void shouldFindNeedleAtEnd_2() {
+		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack2(
+				"neevsddleazklneelacsaeqbacszgneedle", "needle");
+
+		assertEquals(29, needleInHaystackPosition);
+	}
+
+	/**
+	 * Same tests as above only targeting the optimised while implementation of
+	 * the getNeedleInAHaystack2() called getNeedleInAHaystack3().
+	 */
+
+	@Test
+	public void shouldFindNeedleAtPosition4_3() {
+		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack2(
+				"azklneedleqbacszg", "needle");
+
+		assertEquals(4, needleInHaystackPosition);
+	}
+
+	@Test
+	public void shouldNotFindNeedle_3() {
+		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack2(
+				"azklneeleqbacszg", "needle");
+
+		assertEquals(-1, needleInHaystackPosition);
+	}
+
+	@Test
+	public void shouldFindNeedleAtBeginning_3() {
+		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack2(
+				"needleazklneelacsaeqbacszg", "needle");
+
+		assertEquals(0, needleInHaystackPosition);
+	}
+
+	@Test
+	public void shouldFindNeedleAtEnd_3() {
+		int needleInHaystackPosition = NeedInAHaystack.getNeedleInAHaystack2(
+				"neevsddleazklneelacsaeqbacszgneedle", "needle");
+
 		assertEquals(29, needleInHaystackPosition);
 	}
 
